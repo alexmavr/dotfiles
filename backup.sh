adbed8d7f2461f3afe1508af0c32ae911d48d2fd
Alex Mavrogiannis <nalfemp@gmail.com>
2012-09-04 15:20:31 +0300

    for i=1..100 write 'I wont forget to backup again'

---
+++ backup.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+cp ~/.xmonad/xmonad.hs .
+cp ~/.xmobarrc .
+cp ~/.xinitrc .
+cp ~/.Xresources .
+cp ~/.bashrc .
--- xmonad.hs
+++ xmonad.hs
@@ -1,27 +1,59 @@
--- Imports
+import Data.List ( isPrefixOf )
 import XMonad
-import XMonad.Hooks.EwmhDesktops
+import System.Exit
+
+import System.IO
+import System.Time
+import System.Locale
+import System.Environment
+
+	--import System.IO.UTF8
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.FadeInactive as FI
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Themes
+import XMonad.Layout.Grid
+import XMonad.Layout.Cross
+import XMonad.Layout.Named
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.DecorationMadness
+import XMonad.Layout.TwoPane
+import XMonad.Layout.LimitWindows
+import XMonad.Layout.Magnifier
+import XMonad.Layout.FixedColumn
+import XMonad.Layout.Combo
+import XMonad.Layout.ComboP
+import XMonad.Layout.Tabbed
+import XMonad.Layout.IM
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.Reflect
+import XMonad.Actions.GridSelect
+import XMonad.Util.NamedScratchpad
+import Data.Ratio ((%))
+import XMonad.Util.Scratchpad 
+import XMonad.Hooks.SetWMName
 
--- Main function
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+pythonDzenPP :: PP
+pythonDzenPP = defaultPP {
+  ppCurrent  = dzenColor "#000001" "#000010" . pad
+    , ppVisible  = dzenColor "#000002" "#000020" . pad
+    , ppHidden   = dzenColor "#000003" "#000030" . pad
+    , ppHiddenNoWindows = dzenColor "#000004" "#000040"
+    , ppUrgent   = dzenColor "#000005" "#000050"
+    , ppWsSep    = ""
+    , ppSep      = ""
+    , ppLayout   = dzenColor "#000007" "#000070" 
+    , ppTitle    = dzenColor "#000006" "#000060". shorten 20
+}
 
--- Bar Spec
-myBar = "xmobar"
-
--- Cystom PP
-myPP = xmobarPP { ppCurrent = xmobarColor"#429242" "" . wrap "<" ">" }
-
--- Key binding 
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
-
--- Main xmonad configuration
-myConfig = defaultConfig{
-	 borderWidth 		= 3
-	, terminal		= "urxvt"
-	, modMask		= mod4Mask
-	, normalBorderColor 	= "#444444"
-	, focusedBorderColor 	= "#164F08"
-	, handleEventHook	= fullscreenEventHook
-	, logHook = dynamicLog
+main = xmonad =<< xmobar defaultConfig 
+	{	terminal = "urxvt"
+	,	modMask = mod4Mask
+	,	borderWidth = 1
 	}
+
