c9939adac462c4e1ff7b7b28c1df272d89a0cfe6
Alex Mavrogiannis <nalfemp@gmail.com>
2012-12-24 21:06:09 +0200

    different approach

--- remote-configs/setup.sh
+++ remote-configs/setup.sh
@@ -5,8 +5,7 @@ cp .zshrc ~/.zshrc
 cd ~
 git clone git://github.com/robbyrussell/oh-my-zsh.git
 
-./oh-my-zsh/tools/install.sh
+mv ~/oh-my-zsh ~/.oh-my-zsh
 
-sleep 10
-mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+chsh -s /usr/bin/zsh
 source ~/.zshrc
